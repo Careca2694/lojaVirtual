9ef87e11c04746a43e5d461683ed600ba3653284
FRIMINO MANUEL CHIFOIO <fmchifoio@gmail.com>
2026-07-24 23:50:04 -0700

    CLientDao created

---
+++ src/model/dao/impl/ClientDao.java
@@ -0,0 +1,15 @@
+package model.dao.impl;
+
+import model.entities.Client;
+
+import java.util.List;
+
+public interface ClientDao {
+
+    public void insert(Client client);
+    public Integer updateById(Client client);
+    public Integer deleteById(int id);
+    Client findById(int id);
+    List<Client> findAll();
+
+}
