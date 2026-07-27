b802ef3fdd89f69256eb3744453bff3be958c455
FRIMINO MANUEL CHIFOIO <fmchifoio@gmail.com>
2026-07-27 05:28:30 -0700

    Implementation Update,insert,deleteById, findById and getClient

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,6 @@
 package application;
 
-import com.mysql.cj.x.protobuf.MysqlxCrud;
+
 import db.DB;
 import model.dao.CategoriaDao;
 import model.dao.ClientDao;
@@ -42,9 +42,17 @@ public class Program {
 //        }
 
         ClientDao clientDao = DaoFactory.createClientDao();
-        Client client = new Client(null,"Firmas",833192002,"Costa do sol",new Date());
-        clientDao.insert(client);
-        System.out.println("Cliente inserido com successo: " + client);
+       // Client client = new Client(null,"Firmas",833192002,"Costa do sol",new Date());
+        //clientDao.insert(client);
+        System.out.println("Cliente inserido com successo: ");
+
+        Client client = clientDao.findById(1);
+        System.out.println("Id encontrado : " + client);
+        Client newClient = new Client(1,"Felisberto",844093368,"Laulane",new Date());
+       clientDao.updateById(newClient);
+        System.out.println("Cliente atualizado com successo: " + newClient);
+        int id = clientDao.deleteById(1);
+        System.out.println("Deleted completed: " + id);
 
 
 
--- src/model/dao/impl/ClientDaoImpl.java
+++ src/model/dao/impl/ClientDaoImpl.java
@@ -7,6 +7,7 @@ import model.entities.Client;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -43,17 +44,79 @@ public class ClientDaoImpl implements ClientDao {
 
     @Override
     public Integer updateById(Client client) {
-        return 0;
+
+        PreparedStatement preparedStatement = null;
+        findById(client.getId());
+
+        try{
+            String sqlUpdateById = "update cliente set nome = ?, nascimento = ?, morada = ?, contacto = ? where id = ?";
+            preparedStatement = connection.prepareStatement(sqlUpdateById);
+
+            preparedStatement.setString(1,client.getName());
+            preparedStatement.setDate(2,new java.sql.Date(client.getBirthDay().getTime()));
+            preparedStatement.setString(3,client.getLocation());
+            preparedStatement.setInt(4,client.getContact());
+            preparedStatement.setInt(5,client.getId());
+            return preparedStatement.executeUpdate();
+
+        }catch(SQLException e){
+            throw new DBException(e.getMessage());
+        }finally {
+            DB.closeStatment(preparedStatement);
+        }
     }
 
     @Override
     public Integer deleteById(int id) {
-        return 0;
+        PreparedStatement preparedStatement = null;
+        findById(id);
+        try{
+            String sqlDeleteById = "Delete from lojavirtual.cliente where id = ?";
+            preparedStatement = connection.prepareStatement(sqlDeleteById);
+            preparedStatement.setInt(1,id);
+            return preparedStatement.executeUpdate();
+        }catch(SQLException e){
+            throw new DBException(e.getMessage());
+        }finally {
+            DB.closeStatment(preparedStatement);
+        }
     }
 
     @Override
     public Client findById(int id) {
-        return null;
+
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try{
+            String sqlFindById = "Select * from cliente where id = ?";
+            preparedStatement = connection.prepareStatement(sqlFindById);
+            preparedStatement.setInt(1,id);
+            resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                return getCliente(resultSet);
+            }else{
+                throw new DBException("Don't found id");
+            }
+        }catch (SQLException e){
+            throw new DBException(e.getMessage());
+        }finally {
+            DB.closeStatment(preparedStatement);
+            DB.closeResultSet(resultSet);
+        }
+    }
+
+    private Client getCliente(ResultSet resultSet) {
+        try{
+            Client client = new Client();
+            client.setId(resultSet.getInt("id"));
+            client.setName(resultSet.getString("nome"));
+            client.setContact(resultSet.getInt("contacto"));
+            client.setLocation(resultSet.getString("morada"));
+            client.setBirthDay(resultSet.getDate("nascimento"));
+            return client;
+        }catch (SQLException e){
+            throw new DBException(e.getMessage());
+        }
     }
 
     @Override
